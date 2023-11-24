ea09ba38233522219aff3b800f09d4757c935327
zaxel <sae11.z.alex@gmail.com>
2023-11-25 00:23:40 +0200

    footer starter structure and styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,14 @@
 ﻿import React from 'react';
+import styles from './styles.module.css';
 
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <footer className={styles.footer}>
+            <div className={styles.copyrights}>
+                <p className={styles.copyrights__year}>2023</p>
+                <p className={styles.copyrights__text}>all rights reserved</p>
+            </div>
+        </footer>
     );
 };
 
