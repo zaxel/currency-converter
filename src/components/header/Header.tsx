00eafa3416210127a470e66a3a8d37742ca2a2fd
zaxel <sae11.z.alex@gmail.com>
2023-11-25 00:24:01 +0200

    header starter structure and styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,22 @@
 ﻿import React from 'react';
+import styles from './styles.module.css';
+import logo from '../../assets/header/logo.png';
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className={styles.header}>
+            <div className={styles.container}>
+
+                <div className={styles.logo_container}>
+                    <div className={styles.logo__img}>
+                        <a href="https:\\google.com">
+                            <img src={logo} alt="logo" width={50} height={40} />
+                        </a>
+                    </div>
+                    <p className={styles.logo__text}>currency converter</p>
+                </div>
+            </div>
+        </header>
     );
 };
 
