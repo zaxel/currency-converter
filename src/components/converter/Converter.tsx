a7be2bb38e29ae250128bdacfe0a6a406c42ec5d
zaxel <sae11.z.alex@gmail.com>
2023-11-26 14:49:39 +0200

    currencies converter fieldset

--- src/components/converter/Converter.tsx
+++ src/components/converter/Converter.tsx
@@ -1,49 +1,25 @@
-﻿import React, { useState } from 'react';
+﻿import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import styles from './styles.module.css';
 import swap from '../../assets/converter/swap.png';
+import Fieldset from '../fieldsetConverter/Fieldset';
 
 
 const Converter = () => {
-    const [inputNumber, setInputNumber] = useState('1');
-
-    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputNumber(Number(e.target.value).toFixed(2));
-    }
+    const optionsLeft: Array<string> = ['UAH', 'USD', 'GBP', 'CHZ', 'PZL'];
+    const optionsRight: Array<string> = ['USD', 'UAH', 'GBP', 'CHZ'];
+    
     return (
         <div className={styles.converter}>
 
 
             <Form className={styles.form}>
-                <fieldset className={styles.f_left}>
-                    <legend>change</legend>
-
-                    <Form.Control value={+inputNumber} onChange={onInputChange} className={styles.input_number} type="number" placeholder="amount" step={0.1} />
-
-                    <Form.Select className={styles.select}>
-                        <option>UAH</option>
-                        <option value="1">USD</option>
-                        <option value="2">GBP</option>
-                        <option value="3">CHZ</option>
-                    </Form.Select>
-                </fieldset>
+                <Fieldset legend={'change'} options={optionsLeft} initNumber={1} isDisabled={false}/>
                 <Button className={styles.button}>
                     <img width={20} height={20} src={swap} alt="swap currencies around" />
                 </Button>
-
-                <fieldset className={styles.f_right}>
-                    <legend>get</legend>
-
-                    <Form.Control className={styles.input_number} type="number" placeholder="amount" />
-
-                    <Form.Select className={styles.select}>
-                        <option>USD</option>
-                        <option value="1">UAH</option>
-                        <option value="2">GBP</option>
-                        <option value="3">CHZ</option>
-                    </Form.Select>
-                </fieldset>
+                <Fieldset legend={'get'} options={optionsRight} initNumber={1} isDisabled={true}/>
             </Form>
         </div>
     );
---
+++ src/components/fieldsetConverter/Fieldset.tsx
@@ -0,0 +1,36 @@
+﻿import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import styles from './styles.module.css';
+
+interface props{
+    legend: string,
+    options: string[],
+    initNumber: number,
+    isDisabled: boolean
+}
+
+const Fieldset = ({legend, options=[], initNumber, isDisabled}: props) => {
+    const [inputNumber, setInputNumber] = useState(initNumber.toString());
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputNumber(Number(e.target.value).toFixed(2));
+    }
+    return (
+        <fieldset>
+            <legend>{legend}</legend>
+
+            <Form.Control disabled={isDisabled} value={+inputNumber} onChange={onInputChange} className={styles.input_number} type="number" placeholder="amount" step={0.1} />
+
+            <Form.Select className={styles.select}>
+                {options.map((op,i)=>{
+                    if(i===0)
+                        return <option>{op}</option>
+                        return <option value={i}>{op}</option> 
+                })}
+                
+            </Form.Select>
+        </fieldset>
+    );
+};
+
+export default Fieldset;
