a2653afc9989e41197151b146049eba79de38935
zaxel <sae11.z.alex@gmail.com>
2023-11-26 13:24:03 +0200

    minor styles cgange to Main item

---
+++ src/components/converter/Converter.tsx
@@ -0,0 +1,52 @@
+﻿import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import styles from './styles.module.css';
+import swap from '../../assets/converter/swap.png';
+
+
+const Converter = () => {
+    const [inputNumber, setInputNumber] = useState('1');
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputNumber(Number(e.target.value).toFixed(2));
+    }
+    return (
+        <div className={styles.converter}>
+
+
+            <Form className={styles.form}>
+                <fieldset className={styles.f_left}>
+                    <legend>change</legend>
+
+                    <Form.Control value={+inputNumber} onChange={onInputChange} className={styles.input_number} type="number" placeholder="amount" step={0.1} />
+
+                    <Form.Select className={styles.select}>
+                        <option>UAH</option>
+                        <option value="1">USD</option>
+                        <option value="2">GBP</option>
+                        <option value="3">CHZ</option>
+                    </Form.Select>
+                </fieldset>
+                <Button className={styles.button}>
+                    <img width={20} height={20} src={swap} alt="swap currencies around" />
+                </Button>
+
+                <fieldset className={styles.f_right}>
+                    <legend>get</legend>
+
+                    <Form.Control className={styles.input_number} type="number" placeholder="amount" />
+
+                    <Form.Select className={styles.select}>
+                        <option>USD</option>
+                        <option value="1">UAH</option>
+                        <option value="2">GBP</option>
+                        <option value="3">CHZ</option>
+                    </Form.Select>
+                </fieldset>
+            </Form>
+        </div>
+    );
+};
+
+export default Converter;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,14 +2,16 @@
 import styles from './styles.module.css';
 import CurList from '../../components/curList/CurList';
 import CurItemHeader from '../../components/curItemHeader/CurItemHeader';
+import Converter from '../../components/converter/Converter';
 
 const Main = () => {
   return (
     <div className={styles.main}>
-
+      <div className="currencies_data">
         <CurItemHeader />
         <CurList />
-      
+      </div>
+       <Converter />
     </div>
   );
 };
