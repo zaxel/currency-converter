2bbf8256d0d42421c0b4e87cbf9bcb1477db6bbc
zaxel <sae11.z.alex@gmail.com>
2023-11-27 12:09:30 +0200

    currencies rates style minor fixes

--- src/components/curItem/CurItem.tsx
+++ src/components/curItem/CurItem.tsx
@@ -6,18 +6,24 @@ import { formateRate } from '../../helpers/formateRate';
 
 type Props = Omit<IRate, 'base_ccy'>;
 
-const CurItem = ({ccy, buy, sale}:Props) => {
+const CurItem = ({ ccy, buy, sale }: Props) => {
     return (
         <li className={styles.cur_item}>
             <div className={styles.cur_item__cur}>
-                <div className={styles.cur_item__flag_l}>
-                    <img src={getFlag(ccy)} alt="currency flag" />
+                <div className={styles.cur_item__l}>
+                    <div className={styles.cur_item__flag_l}>
+                        <img src={getFlag(ccy)} alt="currency flag" />
+                    </div>
+                    <p className={styles.cur_item__name_l}>{ccy}</p>
                 </div>
-                <p className={styles.cur_item__name_l}>CHF /</p>
-                <div className={styles.cur_item__flag_r}>
-                    <img src={ua} alt="currency flag" />
+                <p className={styles.cur_item__slash}>/</p>
+                <div className={styles.cur_item__r}>
+                    <div className={styles.cur_item__flag_r}>
+                        <img src={ua} alt="currency flag" />
+                    </div>
+                    <p className={styles.cur_item__name_r}>UAH</p>
                 </div>
-                <p className={styles.cur_item__name_r}>UAH</p>
+
             </div>
             <div className={styles.cur_item__price}>{formateRate(buy)}</div>
             <div className={styles.cur_item__price}>{formateRate(sale)}</div>
