dfbc46acbe3075517f668578ba2af0bc759d3b04
zaxel <sae11.z.alex@gmail.com>
2023-11-30 21:19:10 +0200

    add state that saves changed values

---
+++ src/store/changedRatesState.tsx
@@ -0,0 +1,27 @@
+﻿import { create } from "zustand"
+import { IRate } from "../interfaces"
+
+type State = {
+    rates: Array<Omit<IRate, 'base_ccy'>> 
+  }
+  
+  type Action = {
+    setRates: (rates: State['rates']) => void
+  }
+  
+  // Create your store, which includes both state and (optionally) actions
+  export const useChangedRatesState = create<State & Action>((set) => ({
+    rates: [{
+      "ccy": "CHF",
+      "buy": "40.72170",
+      "sale": "40.72170"
+  },
+  {
+      "ccy": "CZK",
+      "buy": "",
+      "sale": "1.61170"
+    },],
+    setRates: (rates) => set(() => ({ rates: rates })),  
+    
+  }))
+
