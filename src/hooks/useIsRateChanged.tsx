3e89b6b573bd878ab60e21e77f22c004b00119c7
zaxel <sae11.z.alex@gmail.com>
2023-11-30 21:26:13 +0200

    Hook that checks whether the current rate has been changed previously.

---
+++ src/hooks/useIsRateChanged.tsx
@@ -0,0 +1,13 @@
+﻿import { useChangedRatesState } from "../store/changedRatesState";
+import { OperationsTypes } from "../interfaces";
+
+
+export const useIsRateChanged = (ccy: string, operation: OperationsTypes) => {
+    const changedRates = useChangedRatesState((state) => state.rates);
+    const rate = changedRates.find(el=>el.ccy===ccy);
+    if(rate===undefined)
+        return [false];
+    if(rate[operation]!==undefined)
+        return [true]
+    return [false];  
+  }
