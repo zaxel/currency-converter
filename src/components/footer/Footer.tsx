20d9b2dcf1cc47f2aff4a655a6f8cf47af30b779
zaxel <sae11.z.alex@gmail.com>
2023-12-02 00:42:54 +0200

    get current copyright year

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.copyrights}>
-                <p className={styles.copyrights__year}>2023</p>
+                <p className={styles.copyrights__year}>{new Date().getFullYear()}</p>
                 <p className={styles.copyrights__text}>all rights reserved</p>
             </div>
         </footer>
