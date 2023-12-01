c3a9cd31c095b79c05305a10b4fbce9dc435877f
zaxel <sae11.z.alex@gmail.com>
2023-12-01 23:15:08 +0200

    CurRate component decomposition. decomposite function that checkes limits of enterd number into separate function.

--- src/components/curRate/CurRate.tsx
+++ src/components/curRate/CurRate.tsx
@@ -1,26 +1,19 @@
-﻿import React, { useState, useEffect, useRef } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import { formateRate } from '../../helpers/formateRate';
-import styles from './styles.module.css';
-import { Form } from 'react-bootstrap';
-
-import { useIsRateChanged } from '../../hooks/useIsRateChanged';
 import { ICurRatesProps } from '../../interfaces';
 import { useChangedRatesState } from '../../store/changedRatesState';
 import CurRateInactive from '../curRateInactive/CurRateInactive';
 import CurRateActive from '../curRateActive/CurRateActive';
+import { ifFitsLimits } from '../../helpers/ifFitsLimits';
 
 
 
 const CurRate = ({ rate, operation, ccy}: ICurRatesProps) => {
-    
-    
     const [rateData, setRateData] = useState('0');
     const [displayInput, setDisplayInput] = useState(false);
     const [input, setInput] = useState(formateRate(rate));
     const [displayEditableIcon, setDisplayEditableIcon] = useState(false);
     const [confirmBtnDisabled, setConfirmBtnDisabled] = useState(false);
-    
-     
 
     const addRates = useChangedRatesState((state) => state.addRates);
 
@@ -28,38 +21,18 @@ const CurRate = ({ rate, operation, ccy}: ICurRatesProps) => {
         setRateData(()=>rate);
     },[])
 
-    // const saveToChangedRatesState = () => {
-    //     addRates({ rate: input, operation, ccy})
-    // }
-    
-
-    const ifFitsLimits = (modifiedRate: string) => {
-        const rateNumber = Number(rate);
-        const max = rateNumber + rateNumber*.1;
-        const min = rateNumber - rateNumber*.1;
-        if(Number(modifiedRate)>max || Number(modifiedRate)<min){
-            return false;
-        }
-        return true;
-    }
     const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInput(formateRate(e.target.value));
-        setConfirmBtnDisabled(!ifFitsLimits(e.target.value));
-
-
-
+        setConfirmBtnDisabled(!ifFitsLimits(e.target.value, rate));
     }
     const onStatContainerClick = () => {
         setDisplayInput(true)
     }
-
     const onConfirmBtnClick = () => {
         setRateData(()=>input);
         setDisplayInput(false);
         setDisplayEditableIcon(false);
-
         addRates({ rate: input, operation, ccy});
-
     }
     const onRejectBtnClick = () => {
         setInput(formateRate(rate));
---
+++ src/helpers/ifFitsLimits.tsx
@@ -0,0 +1,9 @@
+﻿export const ifFitsLimits = (modifiedRate: string, rate: string) => {
+    const rateNumber = Number(rate);
+    const max = rateNumber + rateNumber*.1;
+    const min = rateNumber - rateNumber*.1;
+    if(Number(modifiedRate)>max || Number(modifiedRate)<min){
+        return false;
+    }
+    return true;
+}
