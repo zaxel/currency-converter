68fa28248690c0ff18463505738aefc59dc1b83e
zaxel <sae11.z.alex@gmail.com>
2023-12-02 22:59:10 +0200

    Fieldset component covered with basic unit tests

---
+++ src/components/fieldsetConverter/Fieldset.test.jsx
@@ -0,0 +1,73 @@
+﻿// Fieldset.test.js
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Fieldset from './Fieldset';
+
+describe('Fieldset component', () => {
+    test('renders Fieldset component with legend and inputs', () => {
+        // Arrange
+        const props = {
+            onInputChange: jest.fn(),
+            val: 42,
+            onSelectChange: jest.fn(),
+            selectVal: 'Option 1',
+            legend: 'Test Legend',
+            options: ['Option 1', 'Option 2'],
+            isDisabled: false,
+        };
+
+        // Act
+        render(<Fieldset {...props} />);
+
+        // Assert
+        expect(screen.getByText('Test Legend')).toBeInTheDocument();
+        expect(screen.getByPlaceholderText('amount')).toHaveValue(42);
+        expect(screen.getByDisplayValue('Option 1')).toBeInTheDocument();
+    });
+
+    test('calls onInputChange and onSelectChange when inputs change', () => {
+        // Arrange
+        const onInputChange = jest.fn();
+        const onSelectChange = jest.fn();
+        const props = {
+            onInputChange,
+            val: 42,
+            onSelectChange,
+            selectVal: 'Option 1',
+            legend: 'Test Legend',
+            options: ['Option 1', 'Option 2'],
+            isDisabled: false,
+        };
+
+        //Act
+        render(<Fieldset {...props} />);
+        fireEvent.change(screen.getByPlaceholderText('amount'), { target: { value: 50 } });
+        fireEvent.change(screen.getByRole('combobox'), { target: { value: 'Option 2' } });
+        // Assert
+        expect(onInputChange).toHaveBeenCalledWith(expect.anything());
+        expect(onSelectChange).toHaveBeenCalledWith(expect.anything());
+    });
+
+    test('snapshot test', () => {
+        // Arrange
+        const onInputChange = jest.fn();
+        const onSelectChange = jest.fn();
+        const props = {
+            onInputChange,
+            val: 42,
+            onSelectChange,
+            selectVal: 'Option 1',
+            legend: 'Test Legend',
+            options: ['Option 1', 'Option 2'],
+            isDisabled: false,
+        };
+        
+        //Act
+        const view = render(<Fieldset {...props} />);
+        
+        // Assert
+        expect(view).toMatchSnapshot();
+    })
+})
+
+// Add more tests based on your specific requirements
--- src/components/fieldsetConverter/Fieldset.tsx
+++ src/components/fieldsetConverter/Fieldset.tsx
@@ -3,15 +3,20 @@ import Form from 'react-bootstrap/Form';
 import styles from './styles.module.css';
 import { IFieldsetProps } from '../../interfaces';
 
+
+
 const Fieldset = ({onInputChange, val, onSelectChange, selectVal, legend, options=[], isDisabled}: IFieldsetProps) => {
     return (
         <fieldset>
             <legend>{legend}</legend>
+
             <Form.Control disabled={isDisabled} value={+val} onChange={onInputChange} className={styles.input_number} type="number" placeholder="amount" step={0.1} />
+
             <Form.Select value={selectVal} onChange={onSelectChange} className={styles.select}>
                 {options.map((op,i)=>{
                         return <option value={op} key={op}>{op}</option> 
                 })}
+                
             </Form.Select>
         </fieldset>
     );
